d9e940ee8e988a212f0273327658719a32f02ab5
John Schmidt <john.schmidt.h@gmail.com>
2024-07-27 18:01:15 +0200

    Only fetch raylib when building gui

--- build.zig
+++ build.zig
@@ -10,7 +10,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Solve
-
     const solve_exe = b.addExecutable(.{
         .name = "kakuro",
         .root_source_file = b.path("kakuro.zig"),
@@ -22,17 +21,18 @@ pub fn build(b: *std.Build) void {
     solve_exe.root_module.addOptions("build_options", solve_opts);
     solve_opts.addOption(Mode, "mode", .solve);
 
-    b.installArtifact(solve_exe);
     const solve_cmd = b.addRunArtifact(solve_exe);
+    const solve_install = b.addInstallArtifact(solve_exe, .{});
     solve_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         solve_cmd.addArgs(args);
     }
     const solve_step = b.step("solve", "Run TUI solver");
     solve_step.dependOn(&solve_cmd.step);
+    solve_step.dependOn(&solve_install.step);
+
 
     // GUI
-
     const gui_exe = b.addExecutable(.{
         .name = "kakuro-gui",
         .root_source_file = b.path("kakuro.zig"),
@@ -45,18 +45,21 @@ pub fn build(b: *std.Build) void {
     gui_exe.root_module.addOptions("build_options", gui_opts);
     gui_opts.addOption(Mode, "mode", .gui);
 
-    const raylib_zig = b.dependency("raylib_zig", .{
+    const raylib_zig = b.lazyDependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
     });
-    const raylib = raylib_zig.module("raylib");
-    const raylib_artifact = raylib_zig.artifact("raylib");
-    gui_exe.linkLibrary(raylib_artifact);
-    gui_exe.root_module.addImport("raylib", raylib);
+    if (raylib_zig) |rlz| {
+        const raylib = rlz.module("raylib");
+        const raylib_artifact = rlz.artifact("raylib");
+        gui_exe.linkLibrary(raylib_artifact);
+        gui_exe.root_module.addImport("raylib", raylib);
+    }
 
-    b.installArtifact(gui_exe);
+    // b.installArtifact(gui_exe);
     const gui_cmd = b.addRunArtifact(gui_exe);
+    const gui_install = b.addInstallArtifact(gui_exe, .{});
     const gui_step = b.step("gui", "Run TUI gui");
     gui_step.dependOn(&gui_cmd.step);
-    // gui_cmd.step.dependOn(b.getInstallStep());
+    gui_step.dependOn(&gui_install.step);
 }
